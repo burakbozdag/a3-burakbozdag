a42bb9dc5d75a87edb4c83d081c2a171bc50c99d
Burak Bozdağ <bozdagb17@itu.edu.tr>
2017-12-18 22:57:45 +0300

    Searching by input feature and some navigation buttons added.

--- bottle_app.py
+++ bottle_app.py
@@ -15,6 +15,7 @@ for row in reader(input_file):
 import bottle
 from bottle import route, run, Response, default_app, debug, template, get, post, request
 
+
 def htmlify(title,text):
     page = """
         <!doctype html>
@@ -30,6 +31,7 @@ def htmlify(title,text):
     """ % (title,text)
     return page
     
+@route('/csv', 'POST')
 def contents_file():
 	data = """"""
 	for i in range(1,52):
@@ -52,13 +54,73 @@ def contents_file():
 %s
 				</table>
 	""" % (data)
-	return content_table
+	return htmlify("Internet usage percentages in countries",
+                   str(content_table))
 
+
+def form_of_site():
+	site_form = """
+				<form action="/search" method="POST">
+					<fieldset>
+						<legend>Search menu:</legend>
+						Search by country name:<br>
+						<input type="text" name="country_name" value=""><br><br>
+						or<br><br>
+						Search by country code:<br>
+						<input type="text" name="country_code" value=""><br><br>
+						<input type="submit" value="Search"><br>
+					</fieldset>
+				</form>
+	"""
+	return site_form
+	
+@route('/search', 'POST')
+def searching():
+	country_name = request.forms.get('country_name')
+	country_code = request.forms.get('country_code')
+	row_data = """"""
+	if str(country_name) != "":
+		for i in range(1,52):
+			if str(contents[i][0]).casefold() == str(country_name).casefold():
+				for j in range(0,7):
+					row_data += '\t\t\t\t\t\t<td>'+str(contents[i][j])+'</td>\n'
+				break
+	
+	if str(country_code) != "":
+		for i in range(1,52):
+			if str(contents[i][1]).casefold() == str(country_code).casefold():
+				for j in range(0,7):
+					row_data += '\t\t\t\t\t\t<td>'+str(contents[i][j])+'</td>\n'
+				break
+	
+	country_row = """
+				<table>
+					<tr>
+						<th>Country Name</th>
+						<th>Country Code</th>
+						<th>2012</th>
+						<th>2013</th>
+						<th>2014</th>
+						<th>2015</th>
+						<th>2016</th>
+					</tr>
+%s
+				</table>
+	""" % (row_data)
+	return htmlify("Search result", str(country_row))
+
+
+@route ('/')
 def index():
-    return htmlify("Internet usage percentages in countries",
-                   str(contents_file()))
+	return htmlify("Internet usage percentages in countries", 
+					str(form_of_site()) + 
+					"""
+						<br>
+						<form action="/csv" method="POST" target="_blank">
+							<input type="submit" value="Show the csv file">
+						</form>
+					""")
 
-route('/', 'GET', index)
 
 
 #####################################################################
